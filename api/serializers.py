eda61125e58eaf4f4fde9b36efc7f5f9279b7c71
Jason Rempel <rempeljason0@gmail.com>
2024-03-30 18:38:53 -0600

    Uploading jason's code after tutorial6.

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Room
 
+# Outgoing Serializer, these fields are outputted
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
+
+# Incoming Serializer, these fields are going to but input via url
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
